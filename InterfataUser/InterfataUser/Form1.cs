eea8518d7f7d5cd933167f7818dc20e579cc5556
Maria Gabriela <116893641+kayanrae@users.noreply.github.com>
2024-08-01 21:18:05 +0300

    Add files via upload

---
+++ InterfataUser/InterfataUser/Form1.cs
@@ -0,0 +1,32 @@
+using System.Runtime.InteropServices;
+
+namespace InterfataUser
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr Handle, int
+
+        Msg, int Param1, int Param2);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+    }
+}
